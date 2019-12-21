6a96167728962e5b12a3081c48b346c3006d82ca
Jonathan Williams <jonathanwilliams505@gmail.com>
2019-12-21 07:38:36 +0100

    Updates

---
+++ precache-manifest.2f4c67d69303db83408984860fc0b613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3429152250d98b2a74c16cba38a4a9af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1698a79a466ea2df6b81",
+    "url": "/static/css/main.6be46975.chunk.css"
+  },
+  {
+    "revision": "2dc4a712552bc9a6a056",
+    "url": "/static/js/2.056c688d.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.056c688d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1698a79a466ea2df6b81",
+    "url": "/static/js/main.c6a5f166.chunk.js"
+  },
+  {
+    "revision": "f30db92f7db02173606a",
+    "url": "/static/js/runtime-main.6b3a77ed.js"
+  }
+]);
